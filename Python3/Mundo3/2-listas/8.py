f674809acbf885e2d98d08908078632c2215631e
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2022-05-05 23:47:41 -0300

    cev

--- Python3/Mundo3/2-listas/8.py
+++ Python3/Mundo3/2-listas/8.py
@@ -2,4 +2,27 @@
 A) Quantas pessoas foram cadastradas.
 B) Uma listagem com as pessoas mais pesadas. 
 C) Uma listagem com as pessoas mais leves.
-enumerate??'''
+'''
+
+listaNome = listaPeso = []
+maiorPeso = menorPeso = c = posMaiorPeso = posMenorPeso = 0
+while True:
+    nome=str(input("Nome: "))
+    listaNome+=nome,
+    peso=int(input("Peso: "))
+    c+=1
+    if maiorPeso < peso:
+        maiorPeso=peso
+        posMaiorPeso=c
+    if menorPeso == 0:
+        menorPeso = peso
+        posMenorPeso=c
+    elif menorPeso > peso:
+        menorPeso = peso
+        posMenorPeso=c
+    next=str(input("Continuar? [S/N] ")).upper().strip()[0]
+    if next == "N":
+        break
+print(f"O número de pessoas cadastradas foi de {c}!")
+print(f"O maior peso foi de {listaNome[posMaiorPeso-1]}, pesando {maiorPeso}")
+print(f"O menor peso foi de {listaNome[posMenorPeso-1]}, pesando {menorPeso}")
