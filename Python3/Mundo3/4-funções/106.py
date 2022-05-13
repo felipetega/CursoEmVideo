4f810b4d3f560b7f89e671a7933a5d7ffd9b42a9
Felipe Crema Ribeiro <100156699+felipetega@users.noreply.github.com>
2022-05-13 15:55:04 -0300

    cev

--- Python3/Mundo3/4-funções/106.py
+++ Python3/Mundo3/4-funções/106.py
@@ -1,15 +1,32 @@
 '''Exercício Python 106: Faça um mini-sistema que utilize o Interactive Help do Python. O usuário vai digitar o comando e o manual vai aparecer.
 Quando o usuário digitar a palavra ‘FIM’, o programa se encerrará. Importante: use cores.'''
-def programa(*a):
-    '''
-    Multiplica todos os valores fornecidos e diz o resultado
-    '''
-    c = 0
-    x = 1
-    while c < len(a):
-        x *= a[c]
-        c += 1
-    print(f"O produto de todos os valores foi de {x}")
+from time import sleep
 
 
-programa(1, 3, 5)
+def titulo(a, cor):
+    print(f'{cor}~'*(len(a)+4))
+    print(f'  {a}  ')
+    print('~' * (len(a) + 4))
+
+
+def leia(pergunta):
+    global x
+    x = input(pergunta)
+
+
+def ajuda():
+    while True:
+        titulo('SISTEMA DE AJUDA PyHELP', '\033[1;97;42m')
+        leia('\033[mFunção ou Biblioteca > ')
+        if x == 'fim':
+            titulo('ATE LOGO!', '\033[1;97;41m')
+            sleep(2)
+            break
+        titulo(f"Acessando o manual do comando '{x}'", '\033[1;97;44m')
+        print('\033[;30;107m')
+        help(x)
+
+
+# Programa Principal
+x = ()
+ajuda()
